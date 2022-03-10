f41bf9aa75516973c8602170b1bcd74c0ea938ee
Nourhan Atef <anour2168@gmail.com>
2022-03-10 16:27:41 +0200

    Final

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ window.onscroll = function () {
 var navbar = document.getElementById("navbar");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var sticky = navbar.getBoundingClientRect().height;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
